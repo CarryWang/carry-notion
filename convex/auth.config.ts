d230d2935a82963caa1ecff7332e681d4abf4c85
Carry Wang <design.carrywang@gmail.com>
2024-09-18 22:14:58 +0800

    Add auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://striking-lemming-94.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -3,10 +3,17 @@
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
-import { ModeToggle } from "./mode-toggle";
+import { ModeToggle } from "../../../components/mode-toggle";
+import { Spinner } from "../../../components/spinner";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Ghost } from "lucide-react";
+import Link from "next/link";
 
 const Navbar = () => {
   const scrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
   return (
     <div
@@ -17,6 +24,27 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Contion Free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Cotion</Link>
+            </Button>
+            <UserButton />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
